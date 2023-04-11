adc44efff5010580ec3bb3af295a431aba4accda
byhill <byhill.xi1ej@dralias.com>
2023-04-11 00:03:17 -0600

    Add autosave autocmd

--- nvim/nvim/lua/config/autocommands.lua
+++ nvim/nvim/lua/config/autocommands.lua
@@ -1,8 +1,18 @@
+local augroup = function(name) vim.api.nvim_create_augroup(name, { clear = true }) end
+local autocmd = function(event, opts) vim.api.nvim_create_autocmd(event, opts) end
+
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-
-vim.api.nvim_create_autocmd('TextYankPost', {
+autocmd('TextYankPost', {
   callback = function() vim.highlight.on_yank() end,
   group = highlight_group,
+  pattern = '*',
+})
+
+-- Autosave when no activity occurs
+local autosave = augroup('Autosave')
+autocmd({ 'CursorHold', 'CursorHoldI' }, {
+  command = "silent! :write",
+  group = autosave,
   pattern = '*',
 })
