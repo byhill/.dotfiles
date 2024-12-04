898cffbdb21a416904584c83124456987bcaf837
byhill <byhill.xi1ej@dralias.com>
2024-12-04 07:31:27 -0700

    Add instruction to minimal-config

--- nvim/minimal/init.lua
+++ nvim/minimal/init.lua
@@ -1,3 +1,5 @@
+-- nvim -u ~/.dotfiles/nvim/minimal/init.lua
+
 vim.env.LAZY_STDPATH = ".repro"
 load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
 
