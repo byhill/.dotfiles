789101914b2ce9bcf6d02be7934f089723a440ce
byhill <byhill.xi1ej@dralias.com>
2024-12-10 01:33:24 -0700

    Add formatting rules for python

--- nvim/nvim/lua/plugins/format.lua
+++ nvim/nvim/lua/plugins/format.lua
@@ -5,6 +5,7 @@
 local formatters = {
   julia = { "trim_newlines", lsp_format = "last" },
   lua = { lsp_format = "first" },
+  python = { lsp_format = "first" },
   -- markdown = { "markdownlint-cli2" },
 
   -- All other filetypes NOT configured above.
